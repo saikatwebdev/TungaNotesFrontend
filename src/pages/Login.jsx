2bbebf6c0c4658dfcdafc1709836c210040746be
Saikat Tunga <saikattunga56789@gmail.com>
2025-10-20 18:46:08 +0530

    ""

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+const API_URL = process.env.REACT_APP_API_URL ;
 
 const Login = () => {
   const [email, setEmail] = useState("");
